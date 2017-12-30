aab77ca917541891e1a8a19cee63efb2370427da
Constantin Jacob <cj@tzeejay.com>
2017-12-30 22:15:36 +0100

    Basis KFZ Hersteller in SQL Format

---
+++ database/kfz_hersteller.sql
@@ -0,0 +1,1 @@
+INSERT INTO kfz_hersteller (hersteller_name) VALUES ('Porsche'), ('Mercedes-Benz'), ('BMW'), ('Audi'), ('VW'), ('Lexus'), ('Mini'), ('Ford'), ('Nissan'), ('Ferrari'), ('Lamborghini'), ('Toyota');
